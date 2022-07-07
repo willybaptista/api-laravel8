b4901a7ef774f3ebf58d1b6f369e6b203c6c803b
willybaptista <willy.souza@bild.com.br>
2022-07-06 21:04:39 -0300

    route api users

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/users', [UserController::class, 'index']);
